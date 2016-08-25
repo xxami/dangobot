57661cedc81ca4ef278e787542e5ce8142d7c568
xxami <lilah.willacott@gmail.com>
2016-08-25 20:41:36 +0100

    add simple temp lua tests to observe at triggered mono build

--- dangobot/Program.cs
+++ dangobot/Program.cs
@@ -1,16 +1,11 @@
 ﻿
-using System;
-using NLua;
-
 namespace dangobot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Lua state = new Lua();
-            var res = state.DoString("return 1+1")[0] as double?;
-            Console.WriteLine(res);
+
         }
     }
 }
