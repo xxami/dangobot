768c253e2eeb42ec4617b882bec012eee0e891ec
xxami <lilah.willacott@gmail.com>
2016-08-27 22:05:33 +0100

    add more toml configuration loader tests

--- dangobot.tests/Configuration/DangobotConfigurationTests.cs
+++ dangobot.tests/Configuration/DangobotConfigurationTests.cs
@@ -1,7 +1,8 @@
 ﻿
+using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
+using System.Text;
 using dangobot.Configuration;
 using NUnit.Framework;
 
@@ -18,15 +19,60 @@ namespace dangobot.tests.Configuration
         }
 
         [Test]
-        public void LoadsCompleteConfigurationFile()
+        public void LoadsCompleteConfiguration()
         {
             var tomlData = SourceFromTestCases("Complete.toml");
             DangobotConfiguration.Read(tomlData);
             var config = DangobotConfiguration.Items;
+            Assert.That(config.Slack, Is.Not.Null);
             Assert.That(config.Slack.ApiKey, Is.EqualTo("*insert key here*"));
             Assert.That(config.Slack.Channels.Count, Is.EqualTo(2));
             Assert.That(config.Slack.Channels[0], Is.EqualTo("#general"));
             Assert.That(config.Slack.Channels[1], Is.EqualTo("#random"));
+        }
+
+        [Test]
+        public void LoadsEmptyConfiguration()
+        {
+            var tomlData = new MemoryStream(Encoding.UTF8.GetBytes(""));
+            DangobotConfiguration.Read(tomlData);
+            var config = DangobotConfiguration.Items;
+            Assert.That(config.Slack, Is.Null);
+        }
+
+        [Test]
+        public void LoadsPartialConfiguration()
+        {
+            var tomlData = SourceFromTestCases("Partial.toml");
+            DangobotConfiguration.Read(tomlData);
+            var config = DangobotConfiguration.Items;
+            Assert.That(config.Slack, Is.Not.Null);
+            Assert.That(config.Slack.ApiKey, Is.EqualTo("*insert key here*"));
+            Assert.That(config.Slack.Channels, Is.Null);
+        }
+
+        [Test]
+        public void LoadsPartialConfigurationWithUndefinedData()
+        {
+            var tomlData = SourceFromTestCases("UnknownValues.toml");
+            DangobotConfiguration.Read(tomlData);
+            var config = DangobotConfiguration.Items;
+            Assert.That(config.Slack, Is.Not.Null);
+            Assert.That(config.Slack.ApiKey, Is.EqualTo("*insert key here*"));
+        }
+
+        [Test]
+        public void DoesNotLoadMalformedConfiguration()
+        {
+            var tomlData = new MemoryStream(Encoding.UTF8.GetBytes("[]*9$£"));
+            Assert.Throws<Exception>(() => DangobotConfiguration.Read(tomlData));
+        }
+
+        [Test]
+        public void DoesNotLoadConfigurationWithInvalidDataType()
+        {
+            var tomlData = SourceFromTestCases("MismatchingTypes.toml");
+            Assert.Throws<Exception>(() => DangobotConfiguration.Read(tomlData));
         }
     }
 }
