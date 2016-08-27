1a5968dae90d82ed0c6cc49603fab1ef97b8a0f8
xxami <lilah.willacott@gmail.com>
2016-08-27 15:57:45 +0100

    add starting point for toml configuration

---
+++ dangobot.tests/Configuration/DangobotConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+using System.Reflection;
+using dangobot.Configuration;
+using NUnit.Framework;
+
+namespace dangobot.tests.Configuration
+{
+    [TestFixture]
+    public class DangobotConfigurationTests
+    {
+        private Stream SourceFromTestCases(string fileName)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var res = "dangobot.tests.Configuration.TestCases." + fileName;
+            return asm.GetManifestResourceStream(res);
+        }
+
+        [Test]
+        public void LoadsCompleteConfigurationFile()
+        {
+            var tomlPath = SourceFromTestCases("Complete.toml");
+            DangobotConfiguration.Read(tomlPath);
+            var config = DangobotConfiguration.Items;
+            Assert.That(config.Slack.ApiKey, Is.EqualTo("*insert token here*"));
+        }
+    }
+}
---
+++ dangobot/Configuration/DangobotConfiguration.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+using Nett;
+
+namespace dangobot.Configuration
+{
+    public class DangobotConfiguration
+    {
+        public SlackConfigurationItem Slack { get; set; }
+
+        public static DangobotConfiguration Items { get; private set; }
+        public static void Read(Stream tomlData)
+        {
+            DangobotConfiguration.Items = Toml.ReadStream<DangobotConfiguration>(tomlData);
+        }
+    }
+
+    public class SlackConfigurationItem
+    {
+        public List<string> Channels { get; set; }
+        public string ApiKey { get; set; } 
+    }
+}
