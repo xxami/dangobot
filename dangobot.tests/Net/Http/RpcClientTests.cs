d87a78b88214622ce22ebb2632254a2610ec018f
xxami <lilah.willacott@gmail.com>
2016-08-29 21:43:45 +0100

    add rpc client test

--- dangobot.tests/Net/Http/RpcClientTests.cs
+++ dangobot.tests/Net/Http/RpcClientTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using dangobot.Net.Http;
+using Moq;
 using NUnit.Framework;
 
 namespace dangobot.tests.Net.Http
@@ -13,7 +11,20 @@ namespace dangobot.tests.Net.Http
         [Test]
         public void MakesHttpPostRequest()
         {
-            
+            var methodUrl = "url";
+            var jsonArguments = "args";
+
+            var mock = new Mock<IHttpClient>();
+            mock.Setup(client => client.Post(methodUrl, jsonArguments))
+                .Returns("success");
+
+            var rpcClient = new BasicRpcClient()
+            {
+                HttpClient = mock.Object,
+            };
+
+            var response = rpcClient.Call(methodUrl, jsonArguments);
+            Assert.That(response, Is.EqualTo("success"));
         }
     }
 }
