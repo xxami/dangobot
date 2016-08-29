884ff48cbee841b05d7c386131d9aaa528c48357
xxami <lilah.willacott@gmail.com>
2016-08-29 18:17:51 +0100

    add boilerplate for slack's rtm api init

---
+++ dangobot/Net/Slack/RpcClient.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dangobot.Net.Slack
+{
+    interface IRpcClient
+    {
+        string Call(string method, string jsonArguments);
+    }
+
+    public class BasicRpcClient
+    {
+        public string Call(string methodUrl, string jsonArguments)
+        {
+            using (var web = new WebClient())
+                return web.UploadString(methodUrl, "POST", jsonArguments);
+        }
+    }
+}
