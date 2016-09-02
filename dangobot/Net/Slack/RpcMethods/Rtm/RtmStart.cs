7eb11e700b8f206c092859b0a04d9759c9bc4739
xxami <lilah.willacott@gmail.com>
2016-09-02 22:00:35 +0100

    expand on rtmstart rpc method

--- dangobot/Net/Slack/RpcMethods/Rtm/RtmStart.cs
+++ dangobot/Net/Slack/RpcMethods/Rtm/RtmStart.cs
@@ -7,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace dangobot.Net.Slack.RpcMethods.Rtm
 {
-    public class RtmStart
+    public class RtmStart : IRpcMethod
     {
+        class Arguments
+        {
+            public string ApiToken { get; set; }
+        }
+
+        private Arguments _arguments;
+
+        public RtmStart(string apiToken)
+        {
+            _arguments = new Arguments()
+            {
+                ApiToken = apiToken,
+            };
+        }
+
     }
 }
